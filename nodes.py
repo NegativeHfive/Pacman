0a83243724b73b819dd7c88f95a7e471378695c1
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-06-25 11:05:54 +0200

    nodes file added

---
+++ nodes.py
@@ -0,0 +1,16 @@
+import pygame
+from Vector import Vector2
+from contansts import * 
+
+class Node(object):
+    def __init__(self,x,y):
+        self.position = Vector2(x,y)
+        self.neighbors = {UP:None, DOWN:None, LEFT:None, RIGHT:None}
+    
+    def render(self,screen):
+        for n in self.neighbors.keys():
+            if self.neighbors[n] is not None:
+                line_start = self.position.asTuple()
+                line_end = self.neighbors[n].position.asTuple()
+                pygame.draw.line(screen,WHITE, line_start, line_end, 4)
+                pygame.draw.circle(screen,RED, self.position.asInt(),12)
