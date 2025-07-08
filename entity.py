2bffebd2ed8929d16bd94d28d30bc26f89c2ab22
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-07-08 22:02:54 +0200

    ghosts ai

--- entity.py
+++ entity.py
@@ -19,6 +19,8 @@ class Entity(object):
         self.target = node
         self.visible = True
         self.disablePortal = False
+        self.goal = None
+        self.directionMethod = self.randomDirection
         
     def setPosition(self):
         self.position = self.node.position.copy()
@@ -70,7 +72,7 @@ class Entity(object):
         if self.overshotTarget():
             self.node = self.target
             directions = self.validDirection()
-            direction = self.randomDirection(directions)
+            direction = self.directionMethod(directions)
             if not self.disablePortal:
                 if self.node.neighbors[PORTAL] is not None:
                     self.node = self.node.neighbors[PORTAL]
@@ -93,3 +95,11 @@ class Entity(object):
     
     def randomDirection(self, directions):
         return directions[randint(0, len(directions) -1)]
+    
+    def goalDirection(self, directions):
+        distances = []
+        for direction in directions:
+            vec = self.node.position + self.directions[direction] * TILEWIDTH - self.goal
+            distances.append(vec.magnitudeSquared())
+        index = distances.index(min(distances))
+        return directions[index]
