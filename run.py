8076b1f7fd791c7e597afdd9007fa8214bdd253f
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-07-12 21:04:42 +0200

    life icons

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ from ghosts import GhostGroup
 from fruit import Fruit
 from pauser import Pause
 from text import TextGroup
+from spirits import LifeSprites
 
 class GameController(object):
     def __init__(self):
@@ -23,6 +24,7 @@ class GameController(object):
         self.lives = 5
         self.score = 0
         self.textGroup = TextGroup()
+        self.lifesprites = LifeSprites(self.lives)
         
     def restartGame(self):
         self.lives = 5
@@ -31,6 +33,7 @@ class GameController(object):
         self.fruit = None
         self.startGame()
         self.textGroup.showText(READYTXT)
+        self.lifesprites.resetLives(self.lives)
         
     def resetLevel(self):
         self.pause.paused = True
@@ -126,6 +129,7 @@ class GameController(object):
                 elif ghost.mode.current is not SPAWN:
                     if self.pacman.alive: 
                         self.lives -= 1
+                        self.lifesprites.removeImage()
                         self.pacman.die()
                         self.ghosts.hide()
                         if self.lives <= 0:
@@ -166,6 +170,10 @@ class GameController(object):
         self.pacman.render(self.screen)
         self.ghosts.render(self.screen)
         self.textGroup.render(self.screen)
+        for i in range(len(self.lifesprites.images)):
+            x = self.lifesprites.images[i].get_width() * i
+            y = SCREENHEIGHT - self.lifesprites.images[i].get_height()
+            self.screen.blit(self.lifesprites.images[i], (x, y))
         pygame.display.update()
         
     def checkPelletEvents(self):
